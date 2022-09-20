942fef715630cfbaab17bc7ebf87ae6ebaba8a1b
zzWeeZz <42831999+zzWeeZz@users.noreply.github.com>
2022-09-20 21:24:19 +0200

    l

--- Engine/TGP_Engine/src/EntryPoint.h
+++ Engine/TGP_Engine/src/EntryPoint.h
@@ -6,7 +6,7 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 {
 	uint16_t width = 1280;
 	uint16_t height = 720;
-	Engine::Application::GetWindow()->CreateCallback([=]() {Engine::DX11::Initialize(width, height, false, true); });
+	Engine::Application::GetWindow()->CreateCallback([=]() {Engine::DX11::Initialize(width, height, false, false); });
 	auto app = Engine::CreateApplication();
 	app->Run();
 	delete app;
